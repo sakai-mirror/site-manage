dabc92b27f5be053abf358d9ea3ecaf36f2456c9
zqian@umich.edu <zqian@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-04-13 18:58:15 +0000

    merge r76159 of SAK-18256 into 2.7.x
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/branches/sakai-2.7.x@76161 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -1384,6 +1384,8 @@ public class SiteAction extends PagedResourceActionII {
 			context.put("menu2", bar2);
 
 			pagingInfoToContext(state, context);
+			
+			context.put("allowAddSite",Boolean.valueOf(SiteService.allowAddSite(null)));
 			return (String) getContext(data).get("template") + TEMPLATE[0];
 		case 1:
 			/*
