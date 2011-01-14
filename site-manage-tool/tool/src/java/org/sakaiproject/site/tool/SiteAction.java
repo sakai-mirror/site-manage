989363c2506e2c0724b249ab9ce980ec3fbd6b99
zqian@umich.edu <zqian@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2011-01-14 17:03:33 +0000

    merge SAK-19728 into 2.7.x
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/branches/sakai-2.7.x@87298 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -9217,6 +9217,7 @@ public class SiteAction extends PagedResourceActionII {
 			templateSites = new ArrayList();
 		
 		context.put("templateSites",templateSites);
+		context.put("titleMaxLength", state.getAttribute(STATE_SITE_TITLE_MAX));
 		
 	} // setTemplateListForContext
 	
