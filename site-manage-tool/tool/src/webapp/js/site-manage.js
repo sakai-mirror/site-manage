c394f3b5e7cca33261da36f66c349ca6ab957e5d
gsilver@umich.edu <gsilver@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2012-12-18 18:59:47 +0000

    SAK-16600 - fix proper id assignation to selected tools list on load
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/branches/SAK-16600@117832 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage-tool/tool/src/webapp/js/site-manage.js
+++ site-manage-tool/tool/src/webapp/js/site-manage.js
@@ -413,7 +413,7 @@ var setupCategTools = function(){
         if ($(this).attr('checked')) {
             //console.log(thisToolCat  + ' has a checked tool')
             $(this).next('label').css('font-weight', 'bold');
-            $('#toolSelectionList ul').append('<li class=\"icon-' + thisIdClass.replace(/\./g, '-') + '\" id=\"' + $(this).attr('id').replace(/\./g, '_') + '\">' + $(this).next('label').text() + removeLink + '</li>');
+            $('#toolSelectionList ul').append('<li class=\"icon-' + thisIdClass.replace(/\./g, '-') + '\" id=\"selected_' + $(this).attr('id').replace(/\./g, '_') + '\">' + $(this).next('label').text() + removeLink + '</li>');
             $('#toolHolder').find('#' + thisToolCatEsc).find('ul').show();
             $('#toolHolder').find('#' + thisToolCatEsc).find('h4').find('a').addClass('open');
         }
