587905daf8c0b184f227a94777942f3891e97def
steve.swinsburg@gmail.com <steve.swinsburg@gmail.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2013-01-23 05:03:36 +0000

    SAK-23109 merge trunk r118188
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/branches/sakai-2.8.x@118623 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -2537,6 +2537,7 @@ public class SiteAction extends PagedResourceActionII {
 				
 			if (site != null) {
 				context.put("site", site);
+				context.put("siteTitle", site.getTitle());
 
 				List providerCourseList = (List) state
 						.getAttribute(SITE_PROVIDER_COURSE_LIST);
